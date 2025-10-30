9fe54de60afa26195654a22226a5c4e0b1ba1e8d
chumingwang <82766051+chumingwang@users.noreply.github.com>
2025-10-30 18:26:23 -0400

    mushrooms generator

---
+++ Assets/Scripts/MushroomPhysics.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MushroomPhysics : MonoBehaviour
+{
+    private Rigidbody rb;
+    private XRGrabInteractable grabInteractable;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // Subscribe to grab and release events
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        // When player grabs, disable physics so hand controls position
+        rb.useGravity = false;
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        // When player releases, enable gravity again
+        rb.useGravity = true;
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+        grabInteractable.selectExited.RemoveListener(OnRelease);
+    }
+}
